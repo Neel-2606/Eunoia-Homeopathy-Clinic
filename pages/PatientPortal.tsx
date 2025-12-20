9fe5aac816708665bbfc7b3d808487d09dc1f126
Aryanbuha89 <aryanbuha56@gmail.com>
2025-12-20 22:06:03 +0530

    appointment ui improve

--- pages/PatientPortal.tsx
+++ pages/PatientPortal.tsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { SERVICES, FAQS, FACILITIES, CONTACT_INFO, TESTIMONIALS } from '../constants';
 import TreatmentCard from '../components/TreatmentCard';
 import HealthImagesCarousel from '../components/HealthImagesCarousel';
+import '../styles/HeartButton.css';
 
 const PatientPortal: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState('');
@@ -41,9 +42,106 @@ const PatientPortal: React.FC = () => {
               <div className="flex flex-col sm:flex-row gap-3 sm:gap-4 pt-2 w-full sm:w-auto">
                 <Link
                   to="/book-appointment"
-                  className="w-full sm:w-auto bg-primary hover:bg-rose-700 text-white px-6 sm:px-8 py-4 rounded-full font-bold shadow-lg shadow-rose-200 transition-all transform hover:-translate-y-1 text-center text-base sm:text-base"
+                  className="heart-button w-full sm:w-auto shadow-lg shadow-rose-200"
                 >
-                  Book Appointment
+                  <span className="box">Book Appointment</span>
+
+                  <div className="star-1">
+                    <svg
+                      height="25"
+                      width="25"
+                      viewBox="0 0 512 512"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M427.313,88.686c-47.803-47.803-125.213-47.803-173.016,0l-17.087,17.087l-17.087-17.087
+    c-47.803-47.803-125.213-47.803-173.016,0c-47.803,47.803-47.803,125.213,0,173.016l190.103,190.103
+    c4.88,4.88,11.316,7.322,17.752,7.322c6.435,0,13.871-2.442,18.751-7.322l190.103-190.103
+    C475.116,213.899,475.116,136.489,427.313,88.686z"
+                        fill="#fd1853"
+                      ></path>
+                    </svg>
+                  </div>
+                  <div className="star-2">
+                    <svg
+                      height="20"
+                      width="20"
+                      viewBox="0 0 512 512"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M427.313,88.686c-47.803-47.803-125.213-47.803-173.016,0l-17.087,17.087l-17.087-17.087
+    c-47.803-47.803-125.213-47.803-173.016,0c-47.803,47.803-47.803,125.213,0,173.016l190.103,190.103
+    c4.88,4.88,11.316,7.322,17.752,7.322c6.435,0,13.871-2.442,18.751-7.322l190.103-190.103
+    C475.116,213.899,475.116,136.489,427.313,88.686z"
+                        fill="#fd1853"
+                      ></path>
+                    </svg>
+                  </div>
+                  <div className="star-3">
+                    <svg
+                      height="9"
+                      width="9"
+                      viewBox="0 0 512 512"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M427.313,88.686c-47.803-47.803-125.213-47.803-173.016,0l-17.087,17.087l-17.087-17.087
+    c-47.803-47.803-125.213-47.803-173.016,0c-47.803,47.803-47.803,125.213,0,173.016l190.103,190.103
+    c4.88,4.88,11.316,7.322,17.752,7.322c6.435,0,13.871-2.442,18.751-7.322l190.103-190.103
+    C475.116,213.899,475.116,136.489,427.313,88.686z"
+                        fill="#fd1853"
+                      ></path>
+                    </svg>
+                  </div>
+                  <div className="star-4">
+                    <svg
+                      height="10"
+                      width="10"
+                      viewBox="0 0 512 512"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M427.313,88.686c-47.803-47.803-125.213-47.803-173.016,0l-17.087,17.087l-17.087-17.087
+    c-47.803-47.803-125.213-47.803-173.016,0c-47.803,47.803-47.803,125.213,0,173.016l190.103,190.103
+    c4.88,4.88,11.316,7.322,17.752,7.322c6.435,0,13.871-2.442,18.751-7.322l190.103-190.103
+    C475.116,213.899,475.116,136.489,427.313,88.686z"
+                        fill="#fd1853"
+                      ></path>
+                    </svg>
+                  </div>
+                  <div className="star-5">
+                    <svg
+                      height="20"
+                      width="20"
+                      viewBox="0 0 512 512"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M427.313,88.686c-47.803-47.803-125.213-47.803-173.016,0l-17.087,17.087l-17.087-17.087
+    c-47.803-47.803-125.213-47.803-173.016,0c-47.803,47.803-47.803,125.213,0,173.016l190.103,190.103
+    c4.88,4.88,11.316,7.322,17.752,7.322c6.435,0,13.871-2.442,18.751-7.322l190.103-190.103
+    C475.116,213.899,475.116,136.489,427.313,88.686z"
+                        fill="#fd1853"
+                      ></path>
+                    </svg>
+                  </div>
+                  <div className="star-6">
+                    <svg
+                      height="7"
+                      width="7"
+                      viewBox="0 0 512 512"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M427.313,88.686c-47.803-47.803-125.213-47.803-173.016,0l-17.087,17.087l-17.087-17.087
+    c-47.803-47.803-125.213-47.803-173.016,0c-47.803,47.803-47.803,125.213,0,173.016l190.103,190.103
+    c4.88,4.88,11.316,7.322,17.752,7.322c6.435,0,13.871-2.442,18.751-7.322l190.103-190.103
+    C475.116,213.899,475.116,136.489,427.313,88.686z"
+                        fill="#fd1853"
+                      ></path>
+                    </svg>
+                  </div>
                 </Link>
                 <Link
                   to="/contact"
